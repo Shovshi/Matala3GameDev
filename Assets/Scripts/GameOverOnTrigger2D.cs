3214f0213ecfd6eec69260ec70c2be0e6c47588d
shovshi <shovalnh12@gmail.com>
2024-12-03 09:37:43 +0200

    linter

--- Assets/Scripts/GameOverOnTrigger2D.cs
+++ Assets/Scripts/GameOverOnTrigger2D.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameOverOnTrigger2D : MonoBehaviour
 {
-    [Tooltip("Every object tagged with this tag will trigger game over")]
+   // [Tooltip("Every object tagged with this tag will trigger game over")]
     [SerializeField] string triggeringTag;
 
     private void OnTriggerEnter2D(Collider2D other) {
